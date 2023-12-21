993b1ae56fabbf87763f87fad09223885a7829b6
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2023-12-21 10:49:16 +0100

    Check for required packages moved to function 'check_required_pkgs()'.

--- bash_profile_generic.sh
+++ bash_profile_generic.sh
@@ -15,71 +15,53 @@
 #
 # ==============================================================================
 
+
+# ------------------------------------------------------------------------------
+# Language options
+# ------------------------------------------------------------------------------
 LANG=en_US.UTF-8; export LANG
 CHARSET=UTF-8; export CHARSET
 
-# Set length of command history and bash history file
-HISTSIZE=2000
-HISTFILESIZE=2000
 
-# Check for needed packages
-for app in find freecolor fuse-ext2 lynx most ntfs-3g rsync valgrind wget; do
-    result=$( whereis $app )
-    if [ "$result" != "$app"":" ]; then
+# ------------------------------------------------------------------------------
+# Command history options
+# ------------------------------------------------------------------------------
+HISTSIZE=2000      # Set length of command history
+HISTFILESIZE=2000  # Set length of bash history file
+
+
+# ------------------------------------------------------------------------------
+# Command line text editiong options
+# ------------------------------------------------------------------------------
+set -o vi  # Set Vi as default text editing mode
+
+
+# ------------------------------------------------------------------------------
+# User specific aliases
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# User specific functions
+# ------------------------------------------------------------------------------
+function check_required_pkgs() {
+    ts=$( date '+%Y-%m-%d %k:%M:%S' )
+    printf "%s Checking for required packages ...\\n" "$ts"
+    if [ -f /usr/bin/whereis ]; then
+        apps="find freecolor fuse-ext2 lynx most ntfs-3g rsync valgrind wget"
+        for app in $apps; do
+            result=$( whereis $app )
+            ts=$( date '+%Y-%m-%d %k:%M:%S' )
+            if [ "$result" != "$app"":" ]; then
+                printf "%s App %10s ... NOT INSTALLED\\n" "$ts" "$app"
+            else
+                printf "%s App %10s ... INSTALLED\\n" "$ts" "$app"
+            fi
+        done
+    else
         ts=$( date '+%Y-%m-%d %k:%M:%S' )
-        echo -e "$ts WARNING: Could not find: '$app' ..." >> ./.missing_packages
+        printf "%s Can not find 'whereis' ... ABORTING!\\n" "$ts"
     fi
-done
+}
 
-# Set Vi as default text editing mode
-set -o vi
-
-# Define user specific aliases
-alias cls='clear'
-alias clsa='cls;lsa'
-alias ls='ls -1AFhl --color'
-alias lst='ls -t'
-alias lsa='ls -AFhl --color'
-alias lsta='lsa -t'
-alias rmv='rm -vi'
-alias rdir='rm -dr'
-alias rm~='find -P . -type f -name "*~" -exec rm -vi '"'"'{}'"'"' \;'
-alias rmexc='for file in ./*; do [[ -f "$file" ]] && [[ -x "$file" ]] || continue; rm -vi "$file"; done'
-alias cpv='cp -v'
-if [ -e /usr/local/bin/rsync ]; then
-    alias rsyncv='rsync -avu --progress'
-fi
-alias mvv='mv -v'
-alias dfh='df -Th'
-alias duh='du -hsc'
-alias mount_usb_stick='sudo mount_msdosfs -o longnames -g 0 -m 775 -L ca_ES.UTF-8'
-if [ -e /usr/local/bin/wget ]; then
-    alias getpublicip='echo -e "Public IP Address is:" $(wget -q -O - checkip.dyndns.org | egrep -i -o "[0-9]{3}\.[0-9]{3}\.[0-9]{3}\.[0-9]{3}")';
-else
-    echo -e "WARNING: Package 'wget' not installed.";
-fi
-if [ -e /usr/local/bin/lynx ]; then
-    alias nbs_exchange='lynx -dump http://www.nbs.rs/kursnaListaModul/srednjiKurs.faces?lang=eng | head -n41';
-else
-    echo -e "WARNING: Package 'lynx' not installed.";
-fi
-if [ -e /usr/local/bin/freecolor ]; then
-    alias memory='freecolor -t -m';
-else
-    echo -e "WARNING: Package 'freecolor' not installed.";
-fi
-if [ -e /usr/local/bin/valgrind ]; then
-    alias profilemyapp='G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind --tool=memcheck --leak-check=full';
-else
-    echo -e "WARNING: Package 'valgrind' not installed.";
-fi
-if [ -e /usr/local/bin/fuse-ext2 ]; then
-    alias mount_extfs='sudo fuse-ext2 -o ro,allow_other,gid=0,umask=002';
-else
-    echo -e "WARNING: Package 'fusefs-ext2' not installed.";
-fi
-if [ -e /usr/local/bin/ntfs-3g ]; then
-    alias mount_ntfs='sudo ntfs-3g -o rw';
-else
-    echo -e "WARNING: Package 'fusefs-ntfs' not installed.";
-fi
+check_required_pkgs
